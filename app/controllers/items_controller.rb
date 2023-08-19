9189eb3076e8600fdead9e7bbd898c15b9417a32
sukonvu <yumachan.0905.sukonvuhime@gmail.com>
2023-08-19 15:09:22 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
 before_action :authenticate_user!, only: [:new, :create, :edit]
+before_action :set_items, only: [:show, :edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -21,18 +22,16 @@ before_action :authenticate_user!, only: [:new, :create, :edit]
   end
 
   def show
-    @item = Item.find(params[:id])
+
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless @item.user == current_user
       redirect_to items_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
        redirect_to item_path(@item)
     else
@@ -41,7 +40,6 @@ before_action :authenticate_user!, only: [:new, :create, :edit]
   end
 
   # def destroy
-  #   @item = Item.find(params[:id])
   #   @item.destroy
   #   redirect_to root_path
   # end
@@ -50,5 +48,9 @@ before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def item_params
     params.require(:item).permit(:item_image, :item_name, :description, :condition_id, :category_id, :delivery_charge_id, :prefecture_id, :shipping_date_id, :price ).merge(user_id: current_user.id)
+  end
+
+  def set_items
+    @item = Item.find(params[:id])
   end
 end
